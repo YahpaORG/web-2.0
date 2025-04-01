df1db7ad0aab0ad065926277cd3ab33e967211bc
YAHPA Website <website@yahpa.org>
2025-03-31 21:34:55 -0400

    move theme provider to components

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@/providers/ThemeProvider'
+import { ThemeProvider } from '@/components/providers/ThemeProvider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
