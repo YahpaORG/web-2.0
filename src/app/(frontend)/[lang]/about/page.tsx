a197bbdb6acf1934c28f2b918b5e66fa635dedc4
YAHPA Website <website@yahpa.org>
2025-04-16 09:11:36 -0400

    translate about page

--- src/app/(frontend)/[lang]/about/page.tsx
+++ src/app/(frontend)/[lang]/about/page.tsx
@@ -1,21 +1,17 @@
-export default function AboutPage() {
+import { getTranslations } from '@/lib/get-translations'
+import { PageProps } from '@/types/page-props'
+
+export default async function AboutPage(props: PageProps) {
+  const { lang } = await props.params
+
+  const t = await getTranslations(lang)
+
   return (
-    <section className="flex flex-col justify-center items-center">
+    <section className="flex flex-col items-center justify-center">
       <div className="flex flex-col max-w-xl gap-4 mb-8">
-        <h1 className="text-3xl">About YAHPA</h1>
-        <h2 className="font-bold">
-          Learn about who we are, our goals and our passion for better healthcare
-        </h2>
-        <p>
-          YAHPA (Young Asian Health Professionals Association) was founded in the early days of the
-          COVID-19 pandemic in spring 2020 to support Asian communities in Quebec facing challenges
-          due to misinformation, language barriers, and limited access to healthcare. A dedicated
-          group of healthcare professionals and students came together to create coronavirus
-          information guides, develop community resource lists, and translate health announcements
-          into languages like Mandarin, Cantonese, and Vietnamese. YAHPA aims to improve healthcare
-          access, quality, and equity for Asian seniors and unilingual immigrants, addressing
-          language barriers and reducing disparities in care.
-        </p>
+        <h1 className="text-3xl">{t.about.header}</h1>
+        <h2 className="font-bold">{t.about.subheader}</h2>
+        <p>{t.about.description}</p>
       </div>
     </section>
   )
