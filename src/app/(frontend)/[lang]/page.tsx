6531aa4ce98ea8946af4df79b3950249c3ef6c8d
YAHPA Website <website@yahpa.org>
2025-04-16 09:07:47 -0400

    translate home page

--- src/app/(frontend)/[lang]/page.tsx
+++ src/app/(frontend)/[lang]/page.tsx
@@ -1,19 +1,22 @@
 import { Button } from '@/components/ui/button'
+import { getTranslations } from '@/lib/get-translations'
+import { PageProps } from '@/types/page-props'
 import Link from 'next/link'
 
-export default function HomePage() {
+export default async function HomePage(props: PageProps) {
+  const { lang } = await props.params
+
+  const t = await getTranslations(lang)
+
   return (
-    <section className="flex flex-col justify-center items-center">
+    <section className="flex flex-col items-center justify-center">
       <div className="flex flex-col max-w-xl gap-4 mb-8">
-        <h1 className="text-3xl">Welcome to YAHPA</h1>
-        <h2 className="font-bold">Striving for accessible healthcare</h2>
-        <p>
-          We are a group of young professionals and students from various health fields whose
-          mission is to help and support the Asian community in various health-related issues.
-        </p>
+        <h1 className="text-3xl">{t.home.header}</h1>
+        <h2 className="font-bold">{t.home.subheader}</h2>
+        <p>{t.home.description}</p>
         <div>
           <Button asChild>
-            <Link href="/about">Learn more</Link>
+            <Link href="/about">{t.home.learn}</Link>
           </Button>
         </div>
       </div>
