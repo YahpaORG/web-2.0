b18220e33672379e04ca355f08c62e4250de6f87
YAHPA Website <website@yahpa.org>
2025-04-19 11:28:20 -0400

    translate confirm signup page

--- src/app/(frontend)/[locale]/signup/confirm/page.tsx
+++ src/app/(frontend)/[locale]/signup/confirm/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
 import { getUser } from '@/lib/server/get-user.action'
+import { getTranslations } from 'next-intl/server'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function ConfirmSignUpPage() {
   const user = await getUser()
+  const t = await getTranslations('SignUpPage.ConfirmPage')
 
   if (user) {
     redirect('/account')
@@ -14,15 +16,12 @@ export default async function ConfirmSignUpPage() {
     <div className="flex flex-col items-center justify-center flex-1 w-full gap-2 px-8 ">
       <section className="max-w-xl">
         <div className="flex flex-col items-center justify-center w-full max-w-xl p-6 my-2 border-2 rounded-md">
-          <h3 className="mb-4 text-xl font-bold text-center">Confirm your account by email</h3>
-          <p className="mb-4">
-            An email has been sent to you. Please use the verification link to complete the creation
-            of your account with YAHPA.
-          </p>
-          <p>If you have already clicked on the verification link, please sign in here.</p>
+          <h3 className="mb-4 text-xl font-bold text-center">{t('title')}</h3>
+          <p className="mb-4">{t('description1')}</p>
+          <p>{t('description2')}</p>
           <div className="text-center">
             <Button asChild>
-              <Link href="/login">Login to my YAHPA account</Link>
+              <Link href="/login">{t('login')}</Link>
             </Button>
           </div>
         </div>
