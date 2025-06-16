47b85604538a28fbfbdf4b879b28e02881d2097f
YAHPA Website <website@yahpa.org>
2025-06-16 16:16:40 -0400

    add select component from shadcn/ui to update LocaleSwitcher

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -5,39 +5,54 @@ import { usePathname, useRouter } from '@/i18n/navigation'
 import { useParams } from 'next/navigation'
 import { useLocale, Locale, useTranslations } from 'next-intl'
 import { routing } from '@/i18n/routing'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 export default function LocaleSwitcher() {
   const router = useRouter()
   const pathname = usePathname()
   const currentLocale = useLocale()
-  const t = useTranslations()
+  const t = useTranslations('LocaleSwitcher')
   const [locale, setLocal] = useState(currentLocale)
   const params = useParams()
 
   return (
-    <div>
-      <select
-        onChange={(e) => {
-          const newLocale = e.currentTarget.value as Locale
-          router.replace(
-            // @ts-expect-error -- TypeScript will validate that only known `params`
-            // are used in combination with a given `pathname`. Since the two will
-            // always match for the current route, we can skip runtime checks.
-            { pathname, params },
-            { locale: newLocale },
-          )
-          setLocal(newLocale)
-        }}
-        value={locale}
-      >
-        {routing.locales.map((locale) => {
-          return (
-            <option key={locale} value={locale}>
-              {t(`language.${locale}`)}
-            </option>
-          )
-        })}
-      </select>
-    </div>
+    <Select
+      defaultValue={locale}
+      onValueChange={(value) => {
+        const newLocale = value as Locale
+        router.replace(
+          // @ts-expect-error -- TypeScript will validate that only known `params`
+          // are used in combination with a given `pathname`. Since the two will
+          // always match for the current route, we can skip runtime checks.
+          { pathname, params },
+          { locale: newLocale },
+        )
+        setLocal(newLocale)
+      }}
+    >
+      <SelectTrigger className="min-w-[8rem]">
+        <SelectValue placeholder={t('placeholder')} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>{t('label')}</SelectLabel>
+          {routing.locales.map((locale) => {
+            return (
+              <SelectItem key={locale} value={locale}>
+                {t(`${locale}`)}
+              </SelectItem>
+            )
+          })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   )
 }
