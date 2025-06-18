316ce27ab31476273d8077456edcaccd17b49c63
YAHPA Website <website@yahpa.org>
2025-06-18 17:23:29 -0400

    Update home page section components and create FeatureCarousel, Sponsors and Hero components

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,107 +1,42 @@
-import { Button } from '@/components/ui/button'
-import { getTranslations } from 'next-intl/server'
-import Image from 'next/image'
-import Link from 'next/link'
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel'
 import { CallToAction } from '@/components/CallToAction'
-
-const IMAGES = [
-  { src: '/media/1.jpg' },
-  { src: '/media/2.jpg' },
-  { src: '/media/3.jpg' },
-  { src: '/media/4.jpg' },
-]
+import { FeatureCarousel } from '@/components/FeatureCarousel'
+import { Hero } from '@/components/Hero'
+import { Sponsors } from '@/components/Sponsors'
+import { ZapIcon } from 'lucide-react'
+import { getTranslations } from 'next-intl/server'
 
 export default async function HomePage() {
   const t = await getTranslations('HomePage')
 
   return (
     <div>
-      <section className="relative h-screen overflow-hidden text-white">
-        <div className="absolute inset-0">
-          <Image
-            src="/media/hero.jpg"
-            alt="Background Image"
-            className="object-cover object-center w-full h-full"
-            fill
-          />
-          <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent" />
-          <div className="absolute bottom-0 w-full h-[20rem] bg-gradient-to-t from-black to-transparent" />
-        </div>
-        <div className="relative z-10 flex flex-col items-center justify-start h-full mt-24">
-          <div className="max-w-3xl px-4 text-center">
-            <h1 className="mb-4 text-3xl font-bold leading-tight md:text-5xl">{t('Hero.title')}</h1>
-            <p className="max-w-2xl mx-auto mb-8 text-lg text-center text-gray-300">
-              {t('Hero.description')}
-            </p>
-            <Button asChild variant="secondary" size="lg">
-              <Link href="/about">{t('Hero.learn')}</Link>
-            </Button>
-          </div>
-        </div>
-      </section>
+      <Hero
+        heading={t('Hero.title')}
+        description={t('Hero.description')}
+        imageSrc="/media/hero2.jpg"
+        button={{ text: t('Hero.discover'), url: '/about', icon: <ZapIcon className="size-4" /> }}
+        trustText={t('Hero.trust')}
+      />
+      <FeatureCarousel
+        heading={t('Carousel.title')}
+        description={t('Carousel.description')}
+        imageSlides={[
+          { src: '/media/1.jpg', alt: 'Image 1' },
+          { src: '/media/2.jpg', alt: 'Image 2' },
+          { src: '/media/3.jpg', alt: 'Image 3' },
+          { src: '/media/4.jpg', alt: 'Image 4' },
+        ]}
+      />
 
-      <section className="flex flex-col-reverse items-center gap-8 px-6 my-12 jusify-center lg:flex-row">
-        <div className="flex flex-1">
-          <Carousel className="w-full max-w-sm mx-auto md:max-w-xl">
-            <CarouselContent>
-              {IMAGES.map((image, index) => (
-                <CarouselItem key={index}>
-                  <div className="mx-auto relative w-[24rem] h-[18rem] lg:w-[36rem] lg:h-[26rem]">
-                    <Image src={image.src} alt="" className="object-cover rounded-xl" fill />
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <div className="flex justify-center gap-4 my-4">
-              <CarouselPrevious className="static translate-y-0" />
-              <CarouselNext className="static translate-y-0" />
-            </div>
-          </Carousel>
-        </div>
-        <div className="flex flex-col flex-1 gap-6">
-          <div className="max-w-xl">
-            <h2 className="mb-4 text-3xl font-bold">{t('Carousel.title')}</h2>
-            <p>{t('Carousel.description')}</p>
-          </div>
-        </div>
-      </section>
-      <section className="p-12 my-4">
-        <h2 className="p-6 text-2xl font-semibold text-center">{t('Sponsors.title')}</h2>
-        <div className="flex flex-row flex-wrap items-center justify-center gap-8">
-          <div className="rounded-lg dark:bg-foreground">
-            <Image
-              alt=""
-              src="/media/sponsors/patisseriecocobun.svg"
-              width={320}
-              height={240}
-              className="object-contain"
-            />
-          </div>
-          <div className="rounded-lg dark:bg-foreground">
-            <Image
-              alt=""
-              src="/media/sponsors/preso.webp"
-              width={320}
-              height={240}
-              className="object-contain rounded-lg"
-            />
-          </div>
-          <Image
-            alt=""
-            src="/media/sponsors/kimphat.svg"
-            width={320}
-            height={240}
-            className="object-contain rounded-lg"
-          />
-        </div>
-      </section>
+      <Sponsors
+        title={t('Sponsors.title')}
+        logos={[
+          { src: '/media/sponsors/patisseriecocobun.svg', alt: '' },
+          { src: '/media/sponsors/preso.webp', alt: '' },
+          { src: '/media/sponsors/kimphat.svg', alt: '' },
+        ]}
+      />
+
       <section id="latest">{/* TODO: add news section here */}</section>
       <CallToAction
         heading={t('CTA.title')}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,76 @@
+import { HeartHandshakeIcon, Zap } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+type HeroProps = {
+  icon?: React.ReactNode
+  heading: string
+  description: string
+  button: {
+    text: string
+    icon?: React.ReactNode
+    url: string
+  }
+  trustText?: string
+  imageSrc?: string
+  imageAlt?: string
+} & React.ComponentPropsWithRef<'section'>
+
+export const Hero = ({
+  icon = <HeartHandshakeIcon className="size-6" />,
+  heading = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis lacus vestibulum diam blandit, eu ornare mauris feugiat. Nulla facilisi.',
+  button = {
+    text: 'Discover Features',
+    icon: <Zap className="ml-2 size-4" />,
+    url: 'https://www.shadcnblocks.com',
+  },
+  trustText = 'Lorem ipsum dolor sit amet',
+  imageSrc = 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/placeholder-1.svg',
+  imageAlt = 'placeholder',
+  ...props
+}: HeroProps) => {
+  return (
+    <section className="py-32 overflow-hidden" {...props}>
+      <div className="container p-8 lg:p-16">
+        <div className="flex flex-col gap-5">
+          <div className="relative flex flex-col gap-5">
+            <div
+              style={{
+                transform: 'translate(-50%, -50%)',
+              }}
+              className="absolute top-1/2 left-1/2 -z-10 mx-auto size-[800px] rounded-full border [mask-image:linear-gradient(to_top,transparent,transparent,white,white,white,transparent,transparent)] p-16 md:size-[1300px] md:p-32"
+            >
+              <div className="p-16 border rounded-full size-full md:p-32">
+                <div className="border rounded-full size-full"></div>
+              </div>
+            </div>
+            <span className="flex items-center justify-center mx-auto border rounded-full size-16 md:size-20">
+              {icon}
+            </span>
+            <h2 className="max-w-5xl mx-auto text-3xl font-medium text-center text-balance md:text-6xl">
+              {heading}
+            </h2>
+            <p className="max-w-3xl mx-auto text-center text-muted-foreground md:text-lg">
+              {description}
+            </p>
+            <div className="flex flex-col items-center justify-center gap-3 pt-3 pb-12">
+              <Button size="lg" asChild>
+                <Link href={button.url}>
+                  {button.text} {button.icon}
+                </Link>
+              </Button>
+              {trustText && <div className="text-xs text-muted-foreground">{trustText}</div>}
+            </div>
+          </div>
+          <img
+            src={imageSrc}
+            alt={imageAlt}
+            className="mx-auto h-full max-h-[524px] w-full max-w-5xl rounded-2xl object-cover"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
