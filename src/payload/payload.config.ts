f6735f67b2d97739e0449ed5f9c671bbc2d75bfa
YAHPA Website <website@yahpa.org>
2025-10-19 16:56:04 -0400

    change token env variable for vercelBlobStorage

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -85,7 +85,7 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      token: process.env.BLOB_READ_WRITE_TOKEN,
+      token: process.env.dev_READ_WRITE_TOKEN,
     }),
   ],
 })
