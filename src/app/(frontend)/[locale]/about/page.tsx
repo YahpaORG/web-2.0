685e49886704d7d4a198d7ed340773a1d579d07a
YAHPA Website <website@yahpa.org>
2025-10-19 15:39:13 -0400

    create masonry layout for about page hero and add images

--- src/app/(frontend)/[locale]/about/page.tsx
+++ src/app/(frontend)/[locale]/about/page.tsx
@@ -1,15 +1,52 @@
+import { AspectRatio } from '@radix-ui/react-aspect-ratio'
 import { getTranslations } from 'next-intl/server'
+import Image from 'next/image'
 
 export default async function AboutPage() {
   const t = await getTranslations()
 
   return (
-    <section className="flex flex-col items-center justify-center">
-      <div className="flex flex-col max-w-xl gap-4 mb-8">
-        <h1 className="text-3xl">{t('about.header')}</h1>
-        <h2 className="font-bold">{t('about.subheader')}</h2>
-        <p>{t('about.description')}</p>
-      </div>
-    </section>
+    <>
+      <section className="my-8 md:my-16">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-2 gap-4 xl:grid-cols-4">
+            <div className="grid col-span-2 gap-16">
+              <div className="flex flex-col">
+                <div className="p-6 rounded-lg">
+                  <h1 className="mb-4 text-6xl font-bold">{t('about.header')}</h1>
+                  <h2 className="mb-8 text-2xl font-semibold">{t('about.subheader')}</h2>
+                  <p className="text-lg text-primary">{t('about.description')}</p>
+                </div>
+              </div>
+              <AspectRatio ratio={16 / 9}>
+                <Image className="object-cover rounded-lg" src="/media/1.jpg" alt="" fill />
+              </AspectRatio>
+            </div>
+            <div className="grid gap-4">
+              <AspectRatio ratio={4 / 3}>
+                <Image className="object-cover rounded-lg" src="/media/2.jpg" alt="" fill />
+              </AspectRatio>
+              <AspectRatio ratio={2 / 3}>
+                <Image className="object-cover rounded-lg" src="/media/3.jpg" alt="" fill />
+              </AspectRatio>
+              <AspectRatio ratio={1}>
+                <Image className="object-cover rounded-lg" src="/media/4.jpg" alt="" fill />
+              </AspectRatio>
+            </div>
+            <div className="grid gap-4">
+              <AspectRatio ratio={2 / 3}>
+                <Image className="object-cover rounded-lg" src="/media/5.jpg" alt="" fill />
+              </AspectRatio>
+              <AspectRatio ratio={1}>
+                <Image className="object-cover rounded-lg" src="/media/6.jpg" alt="" fill />
+              </AspectRatio>
+              <AspectRatio ratio={1}>
+                <Image className="object-cover rounded-lg" src="/media/7.jpg" alt="" fill />
+              </AspectRatio>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
